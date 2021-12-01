db1b5a9768a4645c20fb2ff0219a685c38eba660
nos64 <manosov1984@gmail.com>
2021-12-02 03:07:12 +0400

    First day

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 $(function(){
 
+    $('.header-slider').slick({
+        dots: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-top.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-bottom.svg" alt=""></button>',
+        vertical: true,
+    });
+
 });
