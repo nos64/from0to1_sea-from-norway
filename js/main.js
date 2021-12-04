bc33520a82590e662642500b48031bf49bcfb7ca
nos64 <manosov1984@gmail.com>
2021-12-04 04:11:58 +0400

    Day2

--- js/main.js
+++ js/main.js
@@ -5,6 +5,41 @@ $(function(){
         prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-top.svg" alt=""></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-bottom.svg" alt=""></button>',
         vertical: true,
+        responsive: [
+          {
+            breakpoint: 361,
+            settings: {
+              dots: false,
+              arrows: false,
+              autoplay: true,
+              autoplaySpeed: 2000,
+            }
+          }
+        ]
     });
+
+    $('.product__name').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        focusOnSelect: true,
+        asNavFor: '.product__content',
+        vertical: true,
+        // centerMode: true,
+        prevArrow: '<button type="button" class="product-prev"><img src="images/product-prev.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="product-next"><img src="images/product-next.svg" alt=""></button>',
+      });
+      $('.product__content').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        asNavFor: '.product__name',
+        fade: true,
+        arrows: false
+      });
+
+
+      $('.menu__btn').on('click', function(){
+        $('.menu__list').toggleClass('menu__list--ative');
+      });
+
 
 });
